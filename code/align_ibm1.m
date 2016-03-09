dde0bafdf06cabdc491b26c171e49dcded691822
yang ran <yangran1210@gmail.com>
2016-03-08 22:38:03 -0500

    no message

--- code/align_ibm1.m
+++ code/align_ibm1.m
@@ -134,8 +134,10 @@ function AM = initialize(eng, fre)
     end
   end
 
-  AM.(CSC401_A2_DEFNS.SENTSTART).(CSC401_A2_DEFNS.SENTSTART) = 1;
-  AM.(CSC401_A2_DEFNS.SENTEND).(CSC401_A2_DEFNS.SENTEND) = 1;
+  AM.SENTSTART = {};
+  AM.SENTEND = {};
+  AM.SENTSTART.SENTSTART = 1;
+  AM.SENTEND.SENTEND = 1;
 end
 
 function t = em_step(t, eng, fre)
