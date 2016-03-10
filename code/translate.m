3ac8edd3c849af4f70e52826cbd9908de53a9357
yang ran <yangran1210@gmail.com>
2016-03-10 02:56:51 -0500

    no message

--- code/translate.m
+++ code/translate.m
@@ -8,7 +8,7 @@ SENTENDMARK = 'SENTEND';
 lines = textread(file, '%s','delimiter','\n');
 
 for l=1:length(lines)
-    french =  preprocess(lines{l}, language);
+    french =  preprocess(lines{l}, 'f');
 
     disp(strjoin(decode(french, LM, AM, lmtype, delta, vocabSize), ' '));
 end
