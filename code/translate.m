1880b3c89bfa91dd4487aa1d822121c9e80c0d73
yang ran <yangran1210@gmail.com>
2016-03-10 02:55:25 -0500

    no message

---
+++ code/translate.m
@@ -0,0 +1,16 @@
+function translations = translate(file, LM, AM, lmtype, delta, vocabSize)
+
+global CSC401_A2_DEFNS
+
+SENTSTARTMARK = 'SENTSTART'; 
+SENTENDMARK = 'SENTEND';
+
+lines = textread(file, '%s','delimiter','\n');
+
+for l=1:length(lines)
+    french =  preprocess(lines{l}, language);
+
+    disp(strjoin(decode(french, LM, AM, lmtype, delta, vocabSize), ' '));
+end
+
+return
