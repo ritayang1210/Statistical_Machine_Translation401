ef5365191db871e7ee42b206ff62412ddded6170
yang ran <yangran1210@gmail.com>
2016-03-10 03:15:36 -0500

    no message

--- code/evalAlign.m
+++ code/evalAlign.m
@@ -5,25 +5,26 @@
 %  Task 5. 
 
 % some of your definitions
-trainDir     = TODO;
-testDir      = TODO;
-fn_LME       = TODO;
-fn_LMF       = TODO;
-lm_type      = TODO;
-delta        = TODO;
-vocabSize    = TODO; 
-numSentences = TODO;
+trainDir     = '/h/u1/cs401/A2_SMT/data/Hansard/Training';
+testDir      = '/u/cs401/A2_SMT/data/Hansard/Testing';
+fn_LME       = 'fn_LME.mat';
+fn_LMF       = 'fn_LMF.mat';
+lm_type      = '';
+delta        = 0.0;
+vocabSize    = 0.0; 
+numSentences = 30000;
 
 % Train your language models. This is task 2 which makes use of task 1
 LME = lm_train( trainDir, 'e', fn_LME );
 LMF = lm_train( trainDir, 'f', fn_LMF );
 
 % Train your alignment model of French, given English 
-AMFE = align_ibm1( trainDir, numSentences );
+AMFE = align_ibm1( trainDir, numSentences, 10 );
 % ... TODO: more 
 
 % TODO: a bit more work to grab the English and French sentences. 
 %       You can probably reuse your previous code for this  
+vocabSize = length(LME.uni);
 
 % Decode the test sentence 'fre'
 eng = decode( fre, LME, AMFE, 'smooth', delta, vocabSize );
