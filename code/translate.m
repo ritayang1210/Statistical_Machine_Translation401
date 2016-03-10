09580ba1d68749ce572acae265c5ab30e6ba5bd2
yang ran <yangran1210@gmail.com>
2016-03-10 03:20:32 -0500

    no message

--- code/translate.m
+++ code/translate.m
@@ -11,6 +11,7 @@ for l=1:length(lines)
     french =  preprocess(lines{l}, 'f');
     disp(['Translating sentence ''', lines{l}, '''']);
     disp(decode2(french, LM, AM, lmtype, delta, vocabSize));
+    disp('\n');
 end
 
 return
